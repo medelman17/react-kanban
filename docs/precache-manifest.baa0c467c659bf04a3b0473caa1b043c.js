01455c7c6323b2cbc290975ffdf811b1c9d68558
Michael Joseph Edelman <michael@edel.mn>
2018-12-16 21:52:15 -0500

    chore(GitHubPages): hoping to work some magic.

---
+++ docs/precache-manifest.baa0c467c659bf04a3b0473caa1b043c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42c14289ecb720a8c134",
+    "url": "/static/js/main.42c14289.chunk.js"
+  },
+  {
+    "revision": "33f89ba244e2d4f3f98d",
+    "url": "/static/js/1.33f89ba2.chunk.js"
+  },
+  {
+    "revision": "42c14289ecb720a8c134",
+    "url": "/static/css/main.5266ddce.chunk.css"
+  },
+  {
+    "revision": "b70c6221070ff97e9e3ca8857e1641a6",
+    "url": "/index.html"
+  }
+];

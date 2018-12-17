e44cff9169ac1414402caab1c65a412222a4030f
Michael Joseph Edelman <michael@edel.mn>
2018-12-16 21:56:41 -0500

    Updates

---
+++ precache-manifest.2763b740e110593ac87c507ff187f165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec1dba4c7f53d7b74314",
+    "url": "/react-kanban/static/js/runtime~main.ec1dba4c.js"
+  },
+  {
+    "revision": "51f5bfe278d0a5f91c47",
+    "url": "/react-kanban/static/js/main.51f5bfe2.chunk.js"
+  },
+  {
+    "revision": "33f89ba244e2d4f3f98d",
+    "url": "/react-kanban/static/js/1.33f89ba2.chunk.js"
+  },
+  {
+    "revision": "51f5bfe278d0a5f91c47",
+    "url": "/react-kanban/static/css/main.5266ddce.chunk.css"
+  },
+  {
+    "revision": "660d4040d92c4bc0e3e7c458065a4b79",
+    "url": "/react-kanban/index.html"
+  }
+];
